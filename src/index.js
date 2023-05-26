9dac3f74f20c46a80e3160297a77c04c10c77ad4
Andrii Kulyk <andrij.kulyk@gmail.com>
2023-05-26 09:31:15 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <PersistGate persistor={persistor}>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <ThemeProvider theme={theme}>
           <App />
         </ThemeProvider>
