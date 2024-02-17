eadd3101a772e594d81a036926863e24045e0f1a
Andrii Kulyk <andrij.kulyk@gmail.com>
2024-02-17 16:10:57 +0200

    fix

--- src/components/Marketplace/Marketplace.jsx
+++ src/components/Marketplace/Marketplace.jsx
@@ -6,7 +6,7 @@ import { useEffect } from "react";
 const Marketplace = () => {
     const {category} = useParams();
     const {params} = useOutletContext();
-    const [searchParams, setSearchParams] = useSearchParams();
+    const [, setSearchParams] = useSearchParams();
 
     useEffect(() => {
         if (params) {
