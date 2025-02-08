cfb635b00c284637ad18149c4568c8e82459eaa6
Andrii Kulyk <andrij.kulyk@gmail.com>
2025-02-08 11:15:44 +0200

    fixes

--- src/components/Modal/Modal.styled.js
+++ src/components/Modal/Modal.styled.js
@@ -1,47 +1,51 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const ModalBackdrop = styled.div`
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 100%;
-    z-index: 10;
-`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  z-index: 99;
+  background-color: ${p => p.theme.colors.backdrop};
+`;
 
 export const ModalContainer = styled.div`
-    transform: ${p => p.type === 'userMenu' & p.show ? 'translate(0, 0) scale(1)' : 
-                p.type === 'userMenu' & !p.show ? 'translate(50%, -50%) scale(0)' : 
-                p.type === 'mainMenu' & p.show ? 'translate(0, 0)' : 'translate(0, -100%)'
-                };
-    opacity: ${p => p.show ? '1' : '0'};
-    position: ${p => p.position ? p.position : 'fixed'};
-    top: ${p => p.type === 'userMenu' ? '57px' : 0};
-    right: ${p => p.type === 'userMenu' ? '30px' : ''};
-    height: ${p => p.type === 'userMenu' ? 'auto' : '100vh'};
-    width: ${p => p.type === 'userMenu' ? 'auto' : '100vw'};
-    padding: ${p => p.type ? '25px' : 0};
-    background-color: ${p => p.type === 'userMenu' ? p.theme.colors.secondary : p.theme.colors.main};
-    transition: all 500ms ease;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 9;
-    border: ${p => p.theme.borders.main} ${p => p.theme.colors.main};
-    border-bottom-left-radius: ${p => p.type === 'userMenu' ? p.theme.radii.normal : 0};
-    border-bottom-right-radius: ${p => p.type === 'userMenu' ? p.theme.radii.normal : 0};
-    border-top-left-radius: ${p => p.type === 'userMenu' ? p.theme.radii.normal : 0};
+  transform: ${p =>
+    (p.type === 'userMenu') & p.show
+      ? 'translate(0, 0) scale(1)'
+      : (p.type === 'userMenu') & !p.show
+      ? 'translate(50%, -50%) scale(0)'
+      : (p.type === 'mainMenu') & p.show
+      ? 'translate(0, 0)'
+      : 'translate(0, -100%)'};
+  opacity: ${p => (p.show ? '1' : '0')};
+  position: ${p => (p.position ? p.position : 'fixed')};
+  top: ${p => (p.type === 'userMenu' ? '57px' : 0)};
+  right: ${p => (p.type === 'userMenu' ? '30px' : '')};
+  height: ${p => (p.type === 'userMenu' ? 'auto' : '100vh')};
+  width: ${p => (p.type === 'userMenu' ? 'auto' : '100vw')};
+  padding: ${p => (p.type ? '25px' : 0)};
+  background-color: ${p =>
+    p.type === 'userMenu' ? p.theme.colors.secondary : p.theme.colors.main};
+  transition: all 500ms ease;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 100;
+  border: ${p => p.theme.borders.main} ${p => p.theme.colors.main};
+  border-bottom-left-radius: ${p => (p.type === 'userMenu' ? p.theme.radii.normal : 0)};
+  border-bottom-right-radius: ${p => (p.type === 'userMenu' ? p.theme.radii.normal : 0)};
+  border-top-left-radius: ${p => (p.type === 'userMenu' ? p.theme.radii.normal : 0)};
 
-    @media (min-width: 768px) {
-        right: ${p => p.type === 'userMenu' ? '72px' : 0};
-    }
+  @media (min-width: 768px) {
+    right: ${p => (p.type === 'userMenu' ? '72px' : 0)};
+  }
 
-    @media (min-width: 1280px) {
-        right: ${p => p.type === 'userMenu' ? '115px' : 0};
-        top: ${p => p.type === 'userMenu' ? '70px' : 0};
-    }
-`
+  @media (min-width: 1280px) {
+    right: ${p => (p.type === 'userMenu' ? '115px' : 0)};
+    top: ${p => (p.type === 'userMenu' ? '70px' : 0)};
+  }
+`;
 
-export const ModalContent = styled.div`
-    
-`
+export const ModalContent = styled.div``;
--- src/components/NftModal/NftModal.styled.js
+++ src/components/NftModal/NftModal.styled.js
@@ -1,217 +1,216 @@
-import styled from "styled-components";
-import {ReactComponent as Plus} from '../../images/icons/Plus.svg';
+import styled from 'styled-components';
+import { ReactComponent as Plus } from '../../images/icons/Plus.svg';
 
 export const ModalBackdrop = styled.div`
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    height: 100vh;
-`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  z-index: 99;
+  background-color: ${p => p.theme.colors.backdrop};
+`;
 
 export const ModalContainer = styled.div`
-    position: absolute;
-    top: 50%;
-    right: 50%;
-    width: calc(100vw - 60px);
-    height: 70%;
-    overflow: hidden;
-    border-radius: ${p => p.theme.radii.normal};
-    background-color: ${p => p.theme.colors.secondary};
-    padding: 40px;
-    opacity: ${p => p.show ? '1' : 0};
-    transform: ${p => p.show ? 'translate(50%, -50%) scale(1)' : 'translate(50%, -50%) scale(0)'};
-    transition: all 500ms ease;
+  position: absolute;
+  top: 50%;
+  right: 50%;
+  width: calc(100vw - 60px);
+  height: 70%;
+  overflow: hidden;
+  border-radius: ${p => p.theme.radii.normal};
+  background-color: ${p => p.theme.colors.secondary};
+  padding: 40px;
+  opacity: ${p => (p.show ? '1' : 0)};
+  transform: ${p => (p.show ? 'translate(50%, -50%) scale(1)' : 'translate(50%, -50%) scale(0)')};
+  transition: all 500ms ease;
 
-    @media (min-width: 768px) {
-        width: calc(100vw - 144px);
-        height: calc(841px * 0.66);
-    }
+  @media (min-width: 768px) {
+    width: calc(100vw - 144px);
+    height: calc(841px * 0.66);
+  }
 
-    @media (min-width: 1280px) {
-        width: 60%;
-        height: calc(841px * 0.8);
-    }
-`
+  @media (min-width: 1280px) {
+    width: 60%;
+    height: calc(841px * 0.8);
+  }
+`;
 
 export const NftForm = styled.form`
-    display: flex;
-    width: 100%;
-    height: 100%;
-    font-family: ${p => p.theme.fontFamily.main};
-    overflow: hidden;
-`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  font-family: ${p => p.theme.fontFamily.main};
+  overflow: hidden;
+`;
 
 export const NftFormFirst = styled.div`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    transform: ${p => p.show === 1 ? 'translate(0, 0)' : 'translate(-100%, 0)'};
-    transition: all 250ms ease;
-`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  transform: ${p => (p.show === 1 ? 'translate(0, 0)' : 'translate(-100%, 0)')};
+  transition: all 250ms ease;
+`;
 
 export const NftFormSecond = styled.div`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    transform: ${p => p.show === 2 ? 'translate(0, 0)' : 'translate(100%, 0)'};
-    transition: all 250ms ease;
-`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  transform: ${p => (p.show === 2 ? 'translate(0, 0)' : 'translate(100%, 0)')};
+  transition: all 250ms ease;
+`;
 
 export const NftFormLabel = styled.label`
-    display: block;
-    font-family: ${p => p.theme.fontFamily.secondary};
-    font-size: ${p => p.theme.fontSizes.body};
-    line-height: ${p => p.theme.lineHeights.body};
-    color: ${p => p.theme.colors.caption};
-    font-weight: ${p => p.theme.fontWeights.boldest};
-    margin-bottom: 5px;
+  display: block;
+  font-family: ${p => p.theme.fontFamily.secondary};
+  font-size: ${p => p.theme.fontSizes.body};
+  line-height: ${p => p.theme.lineHeights.body};
+  color: ${p => p.theme.colors.caption};
+  font-weight: ${p => p.theme.fontWeights.boldest};
+  margin-bottom: 5px;
 
-    @media (min-width: 1280px) {
-        font-size: ${p => p.theme.fontSizes.h5};
-        line-height: ${p => p.theme.lineHeights.h5};
-    }
+  @media (min-width: 1280px) {
+    font-size: ${p => p.theme.fontSizes.h5};
+    line-height: ${p => p.theme.lineHeights.h5};
+  }
 
-    :not(:first-of-type) {
-        margin-top: 5px;
-    }
-`
-
+  :not(:first-of-type) {
+    margin-top: 5px;
+  }
+`;
 
 export const NftFormInput = styled.input`
-    display: block;
-    width: 100%;
-    outline: none;
-    font-size: ${p => p.theme.fontSizes.body};
-    line-height: ${p => p.theme.lineHeights.body};
-    padding: 6px 12px;
-    border: ${p => p.theme.borders.main} ${p => p.theme.colors.caption};
-    border-radius: ${p => p.theme.radii.normal};
-    margin-bottom: ${p => p.mb ? p.mb : 0};
-    transition: all 150ms ease;
+  display: block;
+  width: 100%;
+  outline: none;
+  font-size: ${p => p.theme.fontSizes.body};
+  line-height: ${p => p.theme.lineHeights.body};
+  padding: 6px 12px;
+  border: ${p => p.theme.borders.main} ${p => p.theme.colors.caption};
+  border-radius: ${p => p.theme.radii.normal};
+  margin-bottom: ${p => (p.mb ? p.mb : 0)};
+  transition: all 150ms ease;
 
-    @media (min-width: 768px) {
-        width: 300px;
-    }
+  @media (min-width: 768px) {
+    width: 300px;
+  }
 
-    &::placeholder {
-        font-family: ${p => p.theme.fontFamily.main};
-        color: ${p => p.theme.colors.caption};
-    };
+  &::placeholder {
+    font-family: ${p => p.theme.fontFamily.main};
+    color: ${p => p.theme.colors.caption};
+  }
 
-    :hover,
-    :focus-visible {
-        border-color: ${p => p.theme.colors.accent};
-    }
-`
+  :hover,
+  :focus-visible {
+    border-color: ${p => p.theme.colors.accent};
+  }
+`;
 
 export const NftFormTextarea = styled.textarea`
-    width: 100%;
-    height: 100px;
-    border-radius: ${p => p.theme.radii.normal};
-    border: ${p => p.theme.borders.main} ${p => p.theme.colors.caption};
-    resize: none;
-    padding: 10px;
-    font-family: inherit;
-    font-size: ${p => p.theme.fontSizes.body};
-    line-height: ${p => p.theme.lineHeights.body};
-    outline: none;
-    transition: all 250ms ease;
+  width: 100%;
+  height: 100px;
+  border-radius: ${p => p.theme.radii.normal};
+  border: ${p => p.theme.borders.main} ${p => p.theme.colors.caption};
+  resize: none;
+  padding: 10px;
+  font-family: inherit;
+  font-size: ${p => p.theme.fontSizes.body};
+  line-height: ${p => p.theme.lineHeights.body};
+  outline: none;
+  transition: all 250ms ease;
 
-    &::placeholder {
-        font-family: ${p => p.theme.fontFamily.main};
-        color: ${p => p.theme.colors.caption};
-    };
+  &::placeholder {
+    font-family: ${p => p.theme.fontFamily.main};
+    color: ${p => p.theme.colors.caption};
+  }
 
-    :hover,
-    :focus-visible {
-        border-color: ${p => p.theme.colors.accent};
-    }
-`
+  :hover,
+  :focus-visible {
+    border-color: ${p => p.theme.colors.accent};
+  }
+`;
 
 export const NftFileContainer = styled.div`
-    width: 100%;
-    height: 200px;
-    margin-bottom: 10px;
-    border-radius: ${p => p.theme.radii.normal};
-    background: ${p => p.preview ? `url(${p.preview})` : 'transparent'};
-    background-repeat: no-repeat;
-    background-size: contain;
-    background-position: center;
+  width: 100%;
+  height: 200px;
+  margin-bottom: 10px;
+  border-radius: ${p => p.theme.radii.normal};
+  background: ${p => (p.preview ? `url(${p.preview})` : 'transparent')};
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
 
-    @media (min-width: 768px) {
-        height: 300px;
-    }
+  @media (min-width: 768px) {
+    height: 300px;
+  }
 
-    @media (min-width: 1280px) {
-        height: 400px;
-    }
-`
+  @media (min-width: 1280px) {
+    height: 400px;
+  }
+`;
 
 export const NftFormFileLabel = styled.label`
-    display: flex;
-    align-items: center;
-    cursor: pointer;
-    position: relative;
-    width: 100%;
-    height: 100%;
-    border: ${p => p.theme.borders.main} ${p => p.theme.colors.caption};
-    border-radius: ${p => p.theme.radii.normal};
-    transition: all 250ms ease;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  position: relative;
+  width: 100%;
+  height: 100%;
+  border: ${p => p.theme.borders.main} ${p => p.theme.colors.caption};
+  border-radius: ${p => p.theme.radii.normal};
+  transition: all 250ms ease;
 
-    :hover,
-    :focus-visible {
-        color: ${p => p.theme.colors.accent};
-        border-color: ${p => p.theme.colors.accent};
-    }
-`
+  :hover,
+  :focus-visible {
+    color: ${p => p.theme.colors.accent};
+    border-color: ${p => p.theme.colors.accent};
+  }
+`;
 
 export const NftFormFileInput = styled.input`
-    opacity: 0;
-    width: 0;
-    height: 0;
-`
+  opacity: 0;
+  width: 0;
+  height: 0;
+`;
 
 export const PlusIcon = styled(Plus)`
-    position: absolute;
-    top: 50%;
-    right: 50%;
-    transform: translate(50%, -50%);
-    width: 50px;
-    height: 50px;
-    fill: ${p => p.theme.colors.text};
-    transition: all 250ms ease;
+  position: absolute;
+  top: 50%;
+  right: 50%;
+  transform: translate(50%, -50%);
+  width: 50px;
+  height: 50px;
+  fill: ${p => p.theme.colors.text};
+  transition: all 250ms ease;
 
-    @media (min-width: 1280px) {
-        width: 70px;
-        height: 70px;
-    }
+  @media (min-width: 1280px) {
+    width: 70px;
+    height: 70px;
+  }
 
-    ${NftFormFileLabel}:hover & {
-        fill: ${p => p.theme.colors.accent};
-    }
-    ${NftFormFileLabel}:focus-visible & {
-        fill: ${p => p.theme.colors.accent};
-    }
-`
+  ${NftFormFileLabel}:hover & {
+    fill: ${p => p.theme.colors.accent};
+  }
+  ${NftFormFileLabel}:focus-visible & {
+    fill: ${p => p.theme.colors.accent};
+  }
+`;
 
-export const InputWrapper = styled.div`
-
-`
+export const InputWrapper = styled.div``;
 
 export const BtnWrapper = styled.div`
-    display: flex;
-    gap: 10px;
-    flex-direction: column;
-    align-items: center;
-    margin-top: 20px;
+  display: flex;
+  gap: 10px;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;
 
-    @media (min-width: 768px) {
-        flex-direction: row;
-        justify-content: space-between;
-    }
-`
+  @media (min-width: 768px) {
+    flex-direction: row;
+    justify-content: space-between;
+  }
+`;
